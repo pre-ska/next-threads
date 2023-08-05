ddc5d8597aab8cd6ed8441d5a27efa616f6f2635
preska <wolfwar@outlook.com>
2023-08-05 14:19:06 +0200

    create thread dopola

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,43 +1,49 @@
-import '../globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import React from "react";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 
-import BottomBar from '@/components/shared/BottomBar';
-import TopBar from '@/components/shared/TopBar';
-import LeftSidebar from '@/components/shared/LeftSidebar';
-import RightSidebar from '@/components/shared/RightSidebar';
+import "../globals.css";
+import LeftSidebar from "@/components/shared/LeftSidebar";
+import BottomBar from "@/components/shared/BottomBar";
+import RightSidebar from "@/components/shared/RightSidebar";
+import TopBar from "@/components/shared/TopBar";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Next Threads',
-  description: 'A Next.js 13 Meta Threads Application',
-}
-
+  title: "Next-Threads",
+  description: "A Next.js 13 Meta Threads application",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
-      <html lang="en">
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark,
+      }}
+    >
+      <html lang='en'>
         <body className={inter.className}>
           <TopBar />
-            <main>
+
+          <main className='flex flex-row'>
             <LeftSidebar />
             <section className='main-container'>
-              <div className="w-full-max-w-4xl">
-                {children}
-              </div>
+              <div className='w-full max-w-4xl'>{children}</div>
             </section>
+            {/* @ts-ignore */}
             <RightSidebar />
           </main>
+
           <BottomBar />
         </body>
       </html>
     </ClerkProvider>
-  )
+  );
 }
