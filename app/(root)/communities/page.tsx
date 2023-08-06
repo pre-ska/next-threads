f946270158c1095cf36cfffb9d3d88dcfbb1c980
preska <wolfwar@outlook.com>
2023-08-06 14:15:20 +0200

    community page

--- app/(root)/communities/page.tsx
+++ app/(root)/communities/page.tsx
@@ -1,33 +1,36 @@
-// import { currentUser } from "@clerk/nextjs";
-// import { redirect } from "next/navigation";
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
 // import Searchbar from "@/components/shared/Searchbar";
 // import Pagination from "@/components/shared/Pagination";
 // import CommunityCard from "@/components/cards/CommunityCard";
 
-// import { fetchUser } from "@/lib/actions/user.actions";
-// import { fetchCommunities } from "@/lib/actions/community.actions";
+import { fetchUser } from "@/lib/actions/user.actions";
+import { fetchCommunities } from "@/lib/actions/community.actions";
 
-async function Page(  ) {
-//   const user = await currentUser();
-//   if (!user) return null;
+async function Page({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | undefined };
+}) {
+  const user = await currentUser();
+  if (!user) return null;
 
-//   const userInfo = await fetchUser(user.id);
-//   if (!userInfo?.onboarded) redirect("/onboarding");
+  const userInfo = await fetchUser(user.id);
+  if (!userInfo?.onboarded) redirect("/onboarding");
 
-//   const result = await fetchCommunities({
-//     searchString: searchParams.q,
-//     pageNumber: searchParams?.page ? +searchParams.page : 1,
-//     pageSize: 25,
-//   });
+  const result = await fetchCommunities({
+    searchString: searchParams.q,
+    pageNumber: searchParams?.page ? +searchParams.page : 1,
+    pageSize: 25,
+  });
 
   return (
     <>
       <h1 className='head-text'>Communities</h1>
 
-      {/* <div className='mt-5'>
- 
-        <Searchbar routeType='communities' />
+      <div className='mt-5'>dfdsfasdf
+        {/* <Searchbar routeType='communities' /> */}
       </div>
 
       <section className='mt-9 flex flex-wrap gap-4'>
@@ -36,20 +39,20 @@ async function Page(  ) {
         ) : (
           <>
             {result.communities.map((community) => (
-     
-              <CommunityCard
-                key={community.id}
-                id={community.id}
-                name={community.name}
-                username={community.username}
-                imgUrl={community.image}
-                bio={community.bio}
-                members={community.members}
-              />
+                <h1>{community.username}</h1>
+            //   <CommunityCard
+            //     key={community.id}
+            //     id={community.id}
+            //     name={community.name}
+            //     username={community.username}
+            //     imgUrl={community.image}
+            //     bio={community.bio}
+            //     members={community.members}
+            //   />
             ))}
           </>
         )}
-      </section> */}
+      </section>
 
       {/* <Pagination
         path='communities'
